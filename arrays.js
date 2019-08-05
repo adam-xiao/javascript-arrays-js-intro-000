d56dff3244ea87567d83f4e7f5441b97dabf4849
Adam Xiao <adam.x.xiao@gmail.com>
2019-08-05 15:32:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray("foo", 1){
-array.unshift()
+function addElementToBeginningOfArray("foo", "1"){
+array.unshift("1")
 }
